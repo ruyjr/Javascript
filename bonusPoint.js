ad62f422c9ec253ebdfdde35d9aa1b093837ac42
ruyjr <ruy@verdeleao.com>
2023-08-25 11:12:02 -0300

    bonusPoint.js

---
+++ bonusPoint.js
@@ -0,0 +1,10 @@
+let bonusPoints = 50
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
